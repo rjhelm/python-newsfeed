b70c68a2e84f771ea55e82d139bd3ee69eb6e2cb
rjhelm <rjhelm@github.io>
2021-10-20 12:59:33 -0600

    created filters and ensured that we shut database down to avoid deploy errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
 from app.routes import home, dashboard
+from app.db import init_db
+from app.utils import filters
 from flask import Flask
+
 
 def create_app(test_config=None):
     # set up app config
     app = Flask(__name__, static_url_path='/')
     app.url_map.strict_slashes = False
     app.config.from_mapping(
-        SECRET_KEY='super_secret_key'
-    )
+          SECRET_KEY='super_secret_key'
+        )
+    
+    app.jinja_env.filters['format_url'] = filters.format_url
+    app.jinja_env.filters['format_date'] = filters.format_date
+    app.jinja_env.filters['format_plural'] = filters.format_plural
     
     @app.route('/hello')
     def hello():
@@ -16,5 +23,7 @@ def create_app(test_config=None):
     # register routes
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+
+    init_db(app)
 
     return app
