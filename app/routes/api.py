c00d345126dc4262be6dfac28a7ea3c2c211610e
rjhelm <rjhelm@github.io>
2021-10-20 21:14:30 -0600

    added the decorator function util to the CRUD operations

--- app/routes/api.py
+++ app/routes/api.py
@@ -2,6 +2,7 @@ from os import execv
 from flask import Blueprint, request, jsonify, session
 from app.models import User, Post, Comment, Vote
 from app.db import get_db
+from app.utils.auth import login_required
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -61,6 +62,7 @@ def login():
 
 
 @bp.route('/comments', methods=['POST'])
+@login_required
 def comment():
     data = request.get_json()
     db = get_db()
@@ -85,6 +87,7 @@ def comment():
 
 
 @bp.route('/posts/upvote', methods=['PUT'])
+@login_required
 def upvote():
     data = request.get_json()
     db = get_db()
@@ -108,6 +111,7 @@ def upvote():
 
 
 @bp.route('/posts', methods=['POST'])
+@login_required
 def create():
     data = request.get_json()
     db = get_db()
@@ -132,6 +136,7 @@ def create():
 
 
 @bp.route('/posts/<id>', methods=['PUT'])
+@login_required
 def update(id):
     data = request.get_json()
     db = get_db()
@@ -151,6 +156,7 @@ def update(id):
 
 
 @bp.route('/posts/<id>', methods=['DELETE'])
+@login_required
 def delete(id):
     db = get_db()
 
