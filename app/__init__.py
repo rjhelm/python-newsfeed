12e49e8770d7ffe300d792cf0cc1b0a67058a2ca
rjhelm <rjhelm@github.io>
2021-10-20 19:46:16 -0600

    created login, signup, logout routes and database storage

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from app.routes import home, dashboard
+from app.routes import home, dashboard, api
 from app.db import init_db
 from app.utils import filters
 from flask import Flask
@@ -15,7 +15,7 @@ def create_app(test_config=None):
     app.jinja_env.filters['format_url'] = filters.format_url
     app.jinja_env.filters['format_date'] = filters.format_date
     app.jinja_env.filters['format_plural'] = filters.format_plural
-    
+
     @app.route('/hello')
     def hello():
         return 'hello world'
@@ -23,6 +23,7 @@ def create_app(test_config=None):
     # register routes
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+    app.register_blueprint(api)
 
     init_db(app)
 
--- app/models/User.py
+++ app/models/User.py
@@ -23,5 +23,13 @@ class User(Base):
     def validate_password(self, key, password):
       assert len(password) > 4
       
-      #encrypt password
+    #encrypt password
       return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    def verify_password(self, password):
+          return bcrypt.checkpw(
+            password.encode('utf-8'),
+            self.password.encode('utf-8')
+          )
+      
+      
